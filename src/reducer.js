cf50fe0ebcc69a034273b95968925154e255050b
Jason Yuen <jasony@nulogy.com>
2015-12-13 15:27:07 -0500

    fixes bug with pushing data into the state

--- src/reducer.js
+++ src/reducer.js
@@ -3,7 +3,7 @@ import {Map, fromJS} from 'immutable';
 export default function(state = fromJS({buzzes: []}), action) {
   switch (action.type) {
     case 'ADD_BUZZ_RESULT':
-      return state.update('buzzes', list => list.push(fromJS(action.buzz)));
+      return state.update('buzzes', list => list.push(action.buzz));
     case 'RESET_BUZZ_SESSION':
       return state.clear();
   }
