8981929ee4da3a3432579ca57b02aac91458ace5
Jason Yuen <jasony@nulogy.com>
2015-12-13 01:58:19 -0500

    adds socket.io

--- src/index.jsx
+++ src/index.jsx
@@ -5,6 +5,7 @@ import {createStore} from 'redux';
 import { addBuzzResult, resetBuzzSession } from './action_creators';
 import {Provider} from 'react-redux';
 import reducer from './reducer';
+import io from 'socket.io-client';
 
 require('./stylesheets/application.scss');
 
@@ -13,20 +14,30 @@ store.dispatch(addBuzzResult({ name: 'jason', winner: true }));
 store.dispatch(addBuzzResult({ name: 'amber', winner: false }));
 store.dispatch(addBuzzResult({ name: 'marlowe', winner: false }));
 
-const buzzes = [
-  { name: 'jason', winner: true },
-  { name: 'amber', winner: false },
-  { name: 'marlowe', winner: false },
-];
-
-//ReactDOM.render(
-//  <Provider store={store}>
-//    <BuzzResultsContainer />,
-//  </Provider>,
-//  document.getElementById('app')
-//);
+const socket = io.connect('localhost:5001');
+socket.on('buzz', function(state) {
+  console.log('receiving buzz');
+});
+socket.on('new-buzz-session', function(state) {
+  console.log('receiving new-buzz-session');
+});
 
 ReactDOM.render(
-  <BuzzResults buzzes={buzzes}/>,
+  <Provider store={store}>
+    <BuzzResultsContainer />
+  </Provider>,
   document.getElementById('app')
 );
+
+//const buzzes = [
+//  { name: 'jason', winner: true },
+//  { name: 'amber', winner: false },
+//  { name: 'marlowe', winner: false },
+//];
+//
+//ReactDOM.render(
+//  <BuzzResults buzzes={buzzes}/>,
+//  document.getElementById('app')
+//);
+//
+
