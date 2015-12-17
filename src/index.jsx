394a77ad8a91b1652ddf73856a39e51cfe422113
Jason Yuen <jasony@nulogy.com>
2015-12-16 23:37:37 -0500

    adds audio effects

--- src/index.jsx
+++ src/index.jsx
@@ -20,9 +20,30 @@ const store = compose(
   applyMiddleware(api)
 )(createStore)(reducer);
 
+const audioContext = new AudioContext();
+let audioBuffer;
+const request = new XMLHttpRequest();
+request.open('GET', 'sound2.mp3', true);
+request.responseType = 'arraybuffer';
+request.onload = function () {
+  const undecodedAudio = request.response;
+  audioContext.decodeAudioData(undecodedAudio, function (buffer) {
+    audioBuffer = buffer;
+  });
+};
+request.send();
+
 // setup socket.io connection to listen to async events
 const socket = io.connect('projohn.local:5001');
 socket.on('buzz', function(buzz) {
+  console.log(buzz)
+  if (buzz.winner === true) {
+    const sourceBuffer = audioContext.createBufferSource();
+    sourceBuffer.buffer = audioBuffer;
+    sourceBuffer.connect(audioContext.destination);
+    sourceBuffer.start(audioContext.currentTime);
+  }
+
   store.dispatch(actionCreators.addBuzzResult(buzz));
 });
 socket.on('new-buzz-session', function() {
